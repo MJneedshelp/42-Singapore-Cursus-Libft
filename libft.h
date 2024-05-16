f7c190edd433d5086c5ae519d9bc6da9dda91b2e
MJ 42 <mingjuntan93@gmail.com>
2024-05-16 18:45:18 +0800

    added ft_isalnum

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mintan <mintan@student.42singapore.sg      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 13:37:26 by mintan            #+#    #+#             */
-/*   Updated: 2024/05/14 19:44:52 by mintan           ###   ########.fr       */
+/*   Updated: 2024/05/16 18:35:08 by mintan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 /* Validation Functions*/
 int	ft_isalpha(char c);
 int	ft_isdigit(char c);
+int	ft_isalnum(char c);
 
 /* String Manipulation Functions*/
 int	ft_toupper(char c);
---
+++ main_isalnum.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main_isalnum.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mintan <mintan@student.42singapore.sg      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/14 13:44:28 by mintan            #+#    #+#             */
+/*   Updated: 2024/05/16 18:42:51 by mintan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <ctype.h>
+#include "libft.h"
+
+int main(void)
+{
+	char ch;
+	
+	printf("--------------ft_isalnum test--------------\n");
+	ch = 'A';
+	printf("ch: %c. Testing isalnum: %d | Expected: %d\n", ch, ft_isalnum(ch), isalnum(ch));
+	ch = 'Z';
+	printf("ch: %c. Testing isalnum: %d | Expected: %d\n", ch, ft_isalnum(ch), isalnum(ch));
+	ch = '0';
+	printf("ch: %c. Testing isalnum: %d | Expected: %d\n", ch, ft_isalnum(ch), isalnum(ch));
+	ch = '@';
+	printf("ch: %c. Testing isalnum: %d | Expected: %d\n", ch, ft_isalnum(ch), isalnum(ch));
+	ch = 'a';
+	printf("ch: %c. Testing isalnum: %d | Expected: %d\n", ch, ft_isalnum(ch), isalnum(ch));
+	ch = 'z';
+	printf("ch: %c. Testing isalnum: %d | Expected: %d\n", ch, ft_isalnum(ch), isalnum(ch));
+	ch = 'h';
+	printf("ch: %c. Testing isalnum: %d | Expected: %d\n", ch, ft_isalnum(ch), isalnum(ch));
+	ch = '{';
+	printf("ch: %c. Testing isalnum: %d | Expected: %d\n", ch, ft_isalnum(ch), isalnum(ch));
+	ch = '9';
+	printf("ch: %c. Testing isalnum: %d | Expected: %d\n", ch, ft_isalnum(ch), isalnum(ch));
+	ch = '*';
+	printf("ch: %c. Testing isalnum: %d | Expected: %d\n", ch, ft_isalnum(ch), isalnum(ch));
+
+}
+
