723442ec6a007c2fda32af016d50a1322f7bab62
MJ <mingjuntan93@gmail.com>
2024-05-19 16:29:05 +0800

    Edited main_strlcat

--- main_strlcat.c
+++ main_strlcat.c
@@ -16,30 +16,33 @@ int main(void)
 {
 	printf("--------------ft_strlcat test--------------\n");
 	const char src[] = "67890";
-	char dst1[] = "12345";
+	char dst[] = "12345";
+/*	char dst1[] = "12345";
 	char dst2[] = "12345";
 	char dst3[] = "12345";
 	char dst4[] = "12345";
+*/
 	size_t	n;
 	size_t	res;
 
 	n = 11;
-	printf("Original Dst: %s and n: %lu\n", dst1, n);
+	printf("Original Dst: %s and n: %lu\n", dst, n);
 //	printf("Expected res: %lu | Expected dst: %s\n", strlcat(dst1, src, n), dst1);
-//	printf("Actual res: %lu | Actual dst: %s\n", ft_strlcat(dst1, src, n), dst1);
+	printf("Actual res: %lu | Actual dst: %s\n", ft_strlcat(dst, src, n), dst);
+
 
 	n = 10;
-	printf("Original Dst: %s and n: %lu\n", dst2, n);
+	printf("Original Dst: %s and n: %lu\n", dst, n);
 //	printf("Expected res: %lu | Expected dst: %s\n", strlcat(dst2, src, n), dst)2;
-//	printf("Actual res: %lu | Actual dst: %s\n", ft_strlcat(dst2, src, n), dst2);
+	printf("Actual res: %lu | Actual dst: %s\n", ft_strlcat(dst, src, n), dst);
 
 	n = 6;
-	printf("Original Dst: %s and n: %lu\n", dst3, n);
+	printf("Original Dst: %s and n: %lu\n", dst, n);
 //	printf("Expected res: %lu | Expected dst: %s\n", strlcat(dst3, src, n), dst3);
-//	printf("Actual res: %lu | Actual dst: %s\n", ft_strlcat(dst3, src, n), dst3);
+	printf("Actual res: %lu | Actual dst: %s\n", ft_strlcat(dst, src, n), dst);
 
 	n = 5;
-	printf("Original Dst: %s and n: %lu\n", dst4, n);
+	printf("Original Dst: %s and n: %lu\n", dst, n);
 //	printf("Expected res: %lu | Expected dst: %s\n", strlcat(dst4, src, n), dst4);
-//	printf("Actual res: %lu | Actual dst: %s\n", ft_strlcat(dst4, src, n), dst4);
+	printf("Actual res: %lu | Actual dst: %s\n", ft_strlcat(dst, src, n), dst);
 }
