cf612559f444d08cdfb19a65af8524559ed2b845
MJ 42 <mingjuntan93@gmail.com>
2024-05-22 13:08:00 +0800

    added ft_calloc

---
+++ ft_calloc.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mintan <mintan@student.42singapore.sg      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/22 12:59:27 by mintan            #+#    #+#             */
+/*   Updated: 2024/05/22 13:07:24 by mintan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* Description: allocates memory for an array of nmemb elements and of size 
+   bytes each and returns a pointer to the allocated memory. The memory is set
+   to zero. Returns NULL or a unique pointer value that can be free() if nmemb 
+   or size is 0. Returns error if nmemb x size results in integer overflow. */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+
+}
