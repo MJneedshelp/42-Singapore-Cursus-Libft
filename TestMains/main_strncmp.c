62edb9c92aa24691ea5b239c08ada128dc4b3ad4
MJ <mingjuntan93@gmail.com>
2024-05-27 13:35:24 +0800

    Updated ft_strncmp. Convert all to unsigned char before subtracting

--- TestMains/main_strncmp.c
+++ TestMains/main_strncmp.c
@@ -21,23 +21,31 @@ int main(void)
 	const char str4[] = "ABC";
 	const char str5[] = "aBCDE";
 	const char str6[] = "ABCDe";
+	const char str7[] = "test\200";
+	const char str8[] = "test\0";
 	size_t	n;
 
 	n = 4;
 	printf("Comparing s1: %s and s2: %s | n = %d\n", str1, str2, n);
-	printf("Expected results: %d | Actual results: %d\n", strncmp(str1, str2, n), ft_strncmp(str1, str2, n));
+	printf("Expected results: %d | Actual results: %d\n\n", strncmp(str1, str2, n), ft_strncmp(str1, str2, n));
 	printf("Comparing s1: %s and s2: %s | n = %d\n", str1, str3, n);
-	printf("Expected results: %d | Actual results: %d\n", strncmp(str1, str3, n), ft_strncmp(str1, str3, n));
+	printf("Expected results: %d | Actual results: %d\n\n", strncmp(str1, str3, n), ft_strncmp(str1, str3, n));
 	printf("Comparing s1: %s and s2: %s | n = %d\n", str1, str4, n);
-	printf("Expected results: %d | Actual results: %d\n", strncmp(str1, str4, n), ft_strncmp(str1, str4, n));
+	printf("Expected results: %d | Actual results: %d\n\n", strncmp(str1, str4, n), ft_strncmp(str1, str4, n));
 	printf("Comparing s1: %s and s2: %s | n = %d\n", str1, str5, n);
-	printf("Expected results: %d | Actual results: %d\n", strncmp(str1, str5, n), ft_strncmp(str1, str5, n));
+	printf("Expected results: %d | Actual results: %d\n\n", strncmp(str1, str5, n), ft_strncmp(str1, str5, n));
 	printf("Comparing s1: %s and s2: %s | n = %d\n", str1, str6, n);
-	printf("Expected results: %d | Actual results: %d\n", strncmp(str1, str6, n), ft_strncmp(str1, str6, n));
+	printf("Expected results: %d | Actual results: %d\n\n", strncmp(str1, str6, n), ft_strncmp(str1, str6, n));
 	n = 10;
 	printf("Comparing s1: %s and s2: %s | n = %d\n", str1, str3, n);
-	printf("Expected results: %d | Actual results: %d\n", strncmp(str1, str3, n), ft_strncmp(str1, str3, n));
+	printf("Expected results: %d | Actual results: %d\n\n", strncmp(str1, str3, n), ft_strncmp(str1, str3, n));
 	n = 1;
 	printf("Comparing s1: %s and s2: %s | n = %d\n", str1, str2, n);
-	printf("Expected results: %d | Actual results: %d\n", strncmp(str1, str2, n), ft_strncmp(str1, str2, n));
+	printf("Expected results: %d | Actual results: %d\n\n", strncmp(str1, str2, n), ft_strncmp(str1, str2, n));
+	n = 0;
+	printf("Comparing s1: %s and s2: %s | n = %d\n", str1, str2, n);
+	printf("Expected results: %d | Actual results: %d\n\n", strncmp(str1, str2, n), ft_strncmp(str1, str2, n));
+	n = 6;
+	printf("Comparing s1: %s and s2: %s | n = %d\n", str7, str8, n);
+	printf("Expected results: %d | Actual results: %d\n\n", strncmp(str7, str8, n), ft_strncmp(str7, str8, n));
 }
--- ft_strncmp.c
+++ ft_strncmp.c
@@ -24,11 +24,13 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 
 	i = 1;
+	if (n == 0)
+		return (0);
 	while (*s1 == *s2 && i < n && *s1 != '\0')
 	{
 		s1++;
 		s2++;
 		i++;
 	}
-	return (*s1 - *s2);
+	return ((const unsigned char)*s1 - (const unsigned char)*s2);
 }
