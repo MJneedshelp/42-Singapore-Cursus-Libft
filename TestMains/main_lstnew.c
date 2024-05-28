03ace7b0adfdc3bcf7d79a2168370708782a88f3
MJ 42 <mingjuntan93@gmail.com>
2024-05-28 10:49:20 +0800

    fixed ft_substr, ft_lstnew

--- TestMains/main_lstnew.c
+++ TestMains/main_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: mintan <mintan@student.42singapore.sg>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 02:32:08 by mintan            #+#    #+#             */
-/*   Updated: 2024/05/28 02:32:08 by mintan           ###   ########.fr       */
+/*   Updated: 2024/05/28 10:14:45 by mintan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,13 @@ int main(void)
 
 	printf("Content to be added into the original head node: %s\n", cntnt1);
 	node1 = ft_lstnew((void *)cntnt1);
-	printf("Content in the original head node: %s | Next ptr: %p\n\n", node1->content, node1->next);
+	printf("Content in the original head node: %s | Next ptr: %p\n\n", (char *)node1->content, node1->next);
 	printf("Content to be added into the new head node: %s\n", cntnt2);
 	node2 = ft_lstnew((void *)cntnt2);
 	ft_lstadd_front(&node1, node2);
-	printf("New head added. New head node content: %s | New head node Next ptr: %p\n\n", node2->content, node2->next);
-	printf("Use next of new head to find the next node. Next node content: %s | Next node Next ptr: %p\n\n", node2->next->content, node2->next->next);
+	printf("New head added. New head node content: %s | New head node Next ptr: %p\n\n", (char *)node2->content, node2->next);
+	printf("Use next of new head to find the next node. Next node content: %s | Next node Next ptr: %p\n\n", (char *)node2->next->content, node2->next->next);
+	node1->next = node2;
 	printf("No. of nodes in the list: %d\n\n", ft_lstsize(node2));
 
 	return (0);
--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: mintan <mintan@student.42singapore.sg      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/22 16:48:03 by mintan            #+#    #+#             */
-/*   Updated: 2024/05/27 22:20:56 by mintan           ###   ########.fr       */
+/*   Updated: 2024/05/28 10:46:56 by mintan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,23 +22,23 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	size_t	i;
 
 	i = 0;
-	ptr = (char *)malloc((len + 1) * sizeof(char));
+	if (start >= ft_strlen(s))
+		return (ft_strdup(""));
+	if (len <= ft_strlen(s) - start)
+		ptr = (char *)malloc((len + 1) * sizeof(char));
+	else
+		ptr = (char *)malloc((ft_strlen(s) - start + 1) * sizeof(char));
 	if (ptr == NULL)
 		return (NULL);
-	if (start < ft_strlen(s))
+	if (len <= ft_strlen(s) - start)
 	{
-		if (len < ft_strlen(s) - start)
-		{
-			ft_memcpy(ptr, (s + start), len);
-			*(ptr + len) = '\0';
-		}
-		else
-		{
-			ft_memcpy(ptr, (s + start), ft_strlen(s) - start);
-			*(ptr + ft_strlen(s) - start) = '\0';
-		}
+		ft_memcpy(ptr, (s + start), len);
+		*(ptr + len) = '\0';
 	}
 	else
-		*ptr = '\0';
+	{
+		ft_memcpy(ptr, (s + start), ft_strlen(s) - start);
+		*(ptr + ft_strlen(s) - start) = '\0';
+	}
 	return (ptr);
 }
